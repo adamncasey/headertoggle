9d938036338c4c785a241b5107ca2afba1ba0401
Adam Casey <adamncasey@gmail.com>
2017-01-08 15:01:48 +0000

    Tabs to spaces, unix line endings

--- background.js
+++ background.js
@@ -1,36 +1,39 @@
 var exts = {
-	"h": "cpp",
-	"cpp": "h"
+    "h": "cpp",
+    "cpp": "h"
 }
 
 var funcs = {
-	"toggle-h-to-cpp": () => {
-		chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-			var url = new URL(tabs[0].url);
-			
-			var urlparts = url.pathname.split(".");
-			
-			var ext = urlparts[urlparts.length - 1];
-			
-			var newext = exts[ext];
-			
-			if(!newext) {
-				return;
-			}
-			
-			urlparts[urlparts.length - 1] = newext;
-			
-			url.pathname = urlparts.join(".");
-			
-			chrome.tabs.update(tabs.id, {"url": url.toString()});
-		});
-	}
+    "toggle-h-to-cpp": () => {
+        chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+            if(tabs.length == 0) {
+                return;
+            }
+            var url = new URL(tabs[0].url);
+
+            var urlparts = url.pathname.split(".");
+
+            var ext = urlparts[urlparts.length - 1];
+
+            var newext = exts[ext];
+
+            if(!newext) {
+                return;
+            }
+
+            urlparts[urlparts.length - 1] = newext;
+
+            url.pathname = urlparts.join(".");
+
+            chrome.tabs.update(tabs.id, {"url": url.toString()});
+        });
+    }
 };
 
 chrome.commands.onCommand.addListener(function(command) {
-	func = funcs[command];
-	
-	if(func) {
-		func();
-	}
+    func = funcs[command];
+
+    if(func) {
+        func();
+    }
 });
